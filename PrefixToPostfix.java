4a88dd0156c15a0be0b5c740172e30f27c6ba8d5
shaikji <sjilani2014@gmail.com>
2019-12-15 17:07:19 +0530

    Create PrefixToPostfix.java

---
+++ PrefixToPostfix.java
@@ -0,0 +1,34 @@
+package stacks;
+
+import java.util.Stack;
+
+class PrefixToPostfix {
+
+	static String prefixToPostfix(String prefix) {
+
+		if (prefix == null || prefix.length() == 0)
+			return prefix;
+
+		int n = prefix.length() - 1;
+		Stack<String> stack = new Stack();
+
+		for (int i = n; i >= 0; i--) {
+
+			char ch = prefix.charAt(i);
+			if (Character.isLetterOrDigit(ch)) {
+				stack.push(ch + "");
+			} else {
+				stack.push(stack.pop()  + stack.pop() +ch);
+			}
+		}
+
+		return stack.pop();
+	}
+
+	public static void main(String[] args) {
+		String prefix = "*+AB-CD";
+		System.out.println(prefixToPostfix(prefix));
+		prefix = "*-A/BC-/AKL";
+		System.out.println(prefixToPostfix(prefix));
+	}
+}
