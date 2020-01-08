d3f9a30972f1d0b4ecb90f1d6047861be1786b2f
jilani.shaik <jilani.shaik@servicenow.com>
2020-01-08 16:56:42 +0530

    Greedy Mincoins for change

---
+++ ProblemSolving/src/com/jilani/greedy/MinCoinsChange.java
@@ -0,0 +1,36 @@
+package com.jilani.greedy;
+class MinCoinsChange{
+
+    public static void main(String[] args) {
+        int Value = 93;
+        int[] deno = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+        System.out.println("Min number of coins = " + findMin(deno, Value));
+    }
+
+    static int findMin(int[] deno, int V){
+
+        if ( deno == null || V <= 0){
+            return -1;
+        }
+
+        int n = deno.length;
+        int minCoins = 0;
+
+        for (int i = n-1; i >= 0;){
+            if ( deno[i] == V){
+            		System.out.println(deno[i]);
+                minCoins++;
+                break;
+            } else if ( deno[i] > V){
+                i--;
+            } else {
+        		System.out.println(deno[i]);
+                minCoins++;
+                V = V - deno[i];
+            }
+        }
+        
+        return minCoins;
+
+    }
+}
