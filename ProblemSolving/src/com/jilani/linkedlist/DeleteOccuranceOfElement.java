2146b7230f5560317161693d97f2b15abe278b26
Jilani <sjilani2014@gmail.com>
2020-03-09 22:11:04 +0530

    LinkedList: Delete LastOccurance of an Element

---
+++ ProblemSolving/src/com/jilani/linkedlist/DeleteOccuranceOfElement.java
@@ -0,0 +1,96 @@
+package com.jilani.linkedlist;
+
+public class DeleteOccuranceOfElement {
+
+	public static void main(String[] args) {
+		
+		for ( int i=1; i <= 5; i++) {
+			add(i);
+		}
+		
+		add(5);
+		add(7);
+		add(5);
+		add(9);
+	
+		add(1);
+		printList(head, "Original List");
+		head = deleteLastOccuranceOf(head, 5);
+		printList(head, "List after deleting");
+
+	}
+	
+	static Node deleteLastOccuranceOf(Node head, int k) {
+		
+		if ( head == null)
+			return head;
+		
+		Node toBeDeleted = null;
+		Node prevToDelete = null;
+		
+		Node curr = head;
+		Node prev = null;
+		
+		while ( curr != null) {
+			if ( curr.data == k) {
+				toBeDeleted= curr;
+				prevToDelete = prev; 
+			}
+			prev = curr;
+			curr = curr.next;
+		}
+		
+		if ( toBeDeleted == null) {
+			System.out.println(" Element not found");
+		}
+		
+		if ( prevToDelete == null)
+			head = head.next;
+		else
+			prevToDelete.next = toBeDeleted.next;
+		
+		return head;
+	}
+
+	static class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	static Node head;
+	static Node left;
+
+	static void add(int data) {
+
+		if (head == null) {
+			head = new Node(data);
+			return;
+		}
+
+		Node curr = head;
+
+		while (curr.next != null) {
+			curr = curr.next;
+		}
+		curr.next = new Node(data);
+	}
+	
+	   static void printList(Node head, String message) {
+	        System.out.println(message);
+	        
+	        if (head == null) {
+	        	System.out.println(" List is empty");
+	        	return;
+	        }
+	        while ( head.next != null){
+	            System.out.print(head.data+ " -> ");
+	            head = head.next;
+	        }
+	        System.out.println(head.data);
+	        System.out.println();
+	    }
+}
