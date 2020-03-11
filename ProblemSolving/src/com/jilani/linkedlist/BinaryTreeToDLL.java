a5bab0d7774e4f524e3e6529b18247000011bd4c
Jilani <sjilani2014@gmail.com>
2020-03-11 12:19:49 +0530

    LinkedList: Binary Tree to DLL added one more method

--- ProblemSolving/src/com/jilani/linkedlist/BinaryTreeToDLL.java
+++ ProblemSolving/src/com/jilani/linkedlist/BinaryTreeToDLL.java
@@ -9,12 +9,67 @@ public class BinaryTreeToDLL {
 		root.left = new Node(2);
 		root.left.left = new Node(4);
 		root.left.right = new Node(5);
+		root.left.right.left = new Node(8);
+		root.left.right.right = new Node(9);
+		
 		root.right = new Node(3);
 		root.right.left = new Node(6);
 		root.right.right = new Node(7);
+		root.right.left.left = new Node(10);
+		root.right.left.right = new Node(11);
 		
-		treeToDll(root);
+		//treeToDll(root);
+		start = treeToDllSet1(root);
 		printDLL(start, "DLL");
+	}
+	
+	
+	// Set 1 
+	// Given a 'node',convert the left sub tree, find the inorder predicessor
+	// Weave the inorder predecessor and node
+	// Convert the right subtree
+	// Weave the inorder successor and node
+	// return the node.
+	// Now from node go as left as possible to return the 'head' of the list.
+	
+	static Node treeToDllSet1 (Node root) {
+		
+		if ( root == null)
+			return null;
+		
+		Node node = convertUtil(root);
+		
+		while ( node.left != null)
+			node = node.left;
+		return node;
+	}
+	
+	static Node convertUtil ( Node root) {
+		
+		if ( root == null)
+			return null;
+		
+		if ( root.left != null) {
+			
+			Node left = convertUtil ( root.left);
+			
+			for ( ; left.right != null; ) {
+				left = left.right;
+			}
+			left.right = root;
+			root.left = left;
+		}
+		
+		if ( root.right != null) {
+		
+			Node right = convertUtil ( root.right);
+			for ( ; right.left != null; ) {
+				right = right.left;
+			}
+			right.left = root;
+			root.right = right;
+		}
+		return root;
 	}
 	
 	
