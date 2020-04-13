57455bf2a83aa7e1aefdc598189ee685e4baa39b
Jilani <sjilani2014@gmail.com>
2020-04-13 11:26:24 +0530

    Binary Trees: Reverse a path in a binary tree

--- ProblemSolving/src/com/jilani/queues/ReversePath.java
+++ ProblemSolving/src/com/jilani/queues/ReversePath.java
@@ -1,12 +1,12 @@
 package com.jilani.queues;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class ReversePath {
 
-	
-	
 	public static void main(String[] args) {
 
 		root = new Node(1);
@@ -16,80 +16,115 @@ public class ReversePath {
 		root.left.right = new Node(5);
 		root.right.left = new Node(6);
 		root.right.right = new Node(7);
-		
-		//levelorder(root);
+		root.right.right.right = new Node(10);
+		// levelorder(root);
 		inorder(root);
 		System.out.println();
-		reverse(root, 4, new LinkedList<Integer>());
+		reverse(root, 10, new HashMap<Integer, Integer>(), 0, new INT());
 		System.out.println();
 		inorder(root);
 		System.out.println();
 	}
-	
-	static boolean reverse(Node root, int elem, Queue<Integer> queue) {
+
+	static class INT {
+		int pos;
+	}
+
+	static Node reverse(Node root, int elem, Map<Integer, Integer> map, int level, INT nextpos) {
+		if (root == null)
+			return root;
+
+		map.put(level, root.data);
+
+		if (root.data == elem) {
+			root.data = map.get(nextpos.pos);
+			nextpos.pos++;
+			return root;
+		}
+
+		Node left = null;
+		Node right = null;
+
+		left = reverse(root.left, elem, map, level + 1, nextpos);
+		if (left == null)
+			right = reverse(root.right, elem, map, level + 1, nextpos);
+
+		if ( left != null || right != null) {
+			root.data = map.get(nextpos.pos);
+			nextpos.pos++;
+		}
 		
-		if ( root == null)
+		
+		
+		return left != null? left : right; 
+	}
+
+	static boolean reverseV1(Node root, int elem, Queue<Integer> queue) {
+
+		if (root == null)
 			return false;
 		queue.add(root.data);
-		
+
 		if (root.data == elem) {
 			root.data = queue.poll();
 			return true;
 		}
-		
-		boolean left = reverse(root.left, elem, queue);
-		
+
+		boolean left = reverseV1(root.left, elem, queue);
+
 		if (left) {
 			root.data = queue.poll();
 			return left;
-		} 
-		
-		boolean right = reverse( root.right, elem, queue);
-		
-		if ( right) {
+		}
+
+		boolean right = reverseV1(root.right, elem, queue);
+
+		if (right) {
 			root.data = queue.poll();
 			return right;
 		}
 		queue.poll();
 		return false;
-		
+
 	}
-	
-	static void inorder( Node root) {
-		if( root != null) {
+
+	static void inorder(Node root) {
+		if (root != null) {
 			inorder(root.left);
-			System.out.print(root.data+ " ");
+			System.out.print(root.data + " ");
 			inorder(root.right);
 		}
 	}
-	
+
 	static void levelorder(Node root) {
-		if ( root == null) {
+		if (root == null) {
 			return;
 		}
-		
+
 		Queue<Node> queue = new LinkedList();
 		queue.add(root);
-		
+
 		Node node;
-		while ( !queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			node = queue.poll();
 			System.out.println(node.data);
-			
-			if ( node.left != null)
+
+			if (node.left != null)
 				queue.add(node.left);
-			if ( node.right != null)
+			if (node.right != null)
 				queue.add(node.right);
-			
+
 		}
 	}
 
 	static Node root;
-	static class Node{
+
+	static class Node {
 		Node left;
 		Node right;
 		int data;
-		Node(int data){
+
+		Node(int data) {
 			this.data = data;
 		}
 	}
--- ProblemSolving/src/com/jilani/trees/PrintLevelOrderFromBottomToTop.java
+++ ProblemSolving/src/com/jilani/trees/PrintLevelOrderFromBottomToTop.java
@@ -8,9 +8,9 @@ public class PrintLevelOrderFromBottomToTop {
 
 	public static void main(String[] args) {
 
-		root = new Node(1); // 1
-		root.left = new Node(2); // / \
-		root.right = new Node(3); // 2 3
+		root = new Node(1); 			// 1
+		root.left = new Node(2); 	   // / \
+		root.right = new Node(3);     // 2 3
 		root.left.left = new Node(4); // / \ / \
 		root.left.right = new Node(5); // 4 5 6 7
 		root.right.left = new Node(6);
