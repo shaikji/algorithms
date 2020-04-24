033af090678c6e07ff1f4ab88ddb9790bf804eb7
Jilani <sjilani2014@gmail.com>
2020-04-24 20:54:40 +0530

    BT: Find Diameter of a Binary Tree

---
+++ ProblemSolving/src/com/jilani/trees/checkandprint/DiameterOfATree.java
@@ -0,0 +1,83 @@
+package com.jilani.trees.checkandprint;
+
+public class DiameterOfATree {
+
+	public static void main(String[] args) {
+		
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+
+		root.left.left.left = new Node(8);
+		root.left.left.right = new Node(9);
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(11);
+		root.right.left.left = new Node(12);
+		root.right.left.right = new Node(13);
+		root.right.right.left = new Node(14);
+		root.right.right.right = new Node(15);
+
+		System.out.println(" Diameter = " + diameter(root));
+
+	}
+	
+	static int diameter( Node root ) {
+		
+		if ( root == null)
+			return 0;
+		
+		Height h = new Height();
+		
+		return findDiameter ( root, h);
+		
+	}
+	
+	static int findDiameter ( Node root, Height h) {
+		
+		if ( root == null) {
+			return 0;
+		}
+		
+		if ( root.left == null && root.right == null) {
+			h.height = 1;
+			return 1;
+		}
+		Height lh = new Height();
+		int ld = findDiameter(root.left, lh);
+		
+		Height rh = new Height();
+		int rd = findDiameter(root.right, rh);
+		
+		h.height = 1 + Math.max(lh.height, rh.height);
+		
+		return Math.max(1 + lh.height + rh.height, Math.max(ld, rd));
+	}
+	static class Height{
+		int height;
+		Height() {
+			this.height = 0;
+		}
+		Height(int h) {
+			height = h;
+		}
+	}
+	static Node root;
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	
+
+}
