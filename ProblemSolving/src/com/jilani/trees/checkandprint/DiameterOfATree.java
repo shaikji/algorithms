974e81dc98cf58b8f914c661492a88e790ceee70
Jilani <sjilani2014@gmail.com>
2020-04-24 22:00:47 +0530

    BT: Print leaf to leaf path (Diameter)

--- ProblemSolving/src/com/jilani/trees/checkandprint/DiameterOfATree.java
+++ ProblemSolving/src/com/jilani/trees/checkandprint/DiameterOfATree.java
@@ -10,21 +10,72 @@ public class DiameterOfATree {
 
 		root.left.left = new Node(4);
 		root.left.right = new Node(5);
-		root.right.left = new Node(6);
-		root.right.right = new Node(7);
-
-		root.left.left.left = new Node(8);
-		root.left.left.right = new Node(9);
-		root.left.right.left = new Node(10);
-		root.left.right.right = new Node(11);
-		root.right.left.left = new Node(12);
-		root.right.left.right = new Node(13);
-		root.right.right.left = new Node(14);
-		root.right.right.right = new Node(15);
+		
+		root.left.left.right = new Node(8);
+		root.left.left.right.left = new Node(9);
+		
+		root.left.right.left = new Node(6);
+		root.left.right.right = new Node(7);
+		
 
 		System.out.println(" Diameter = " + diameter(root));
+		
+		printDiameter(diameter_root);
 
 	}
+	
+	static int flag =0;
+	
+	static void printDiameter(Node root) {
+		
+		if ( root == null)
+			return;
+		int[] lpath = new int[100];
+		int pathLen = 0;
+		flag = 0;
+		printPath(root.left, lpath, pathLen, leftHeight);
+		System.out.println(root.data );
+		flag = 1;
+		int[] rpath = new int[100];
+		pathLen = 0;
+		printPath(root.right, rpath, pathLen, rightHeight);
+	}
+	
+	static void printPath ( Node node, int[] path, int pathLen, int height) {
+		
+		if ( node == null)
+			return;
+		
+		path[pathLen++] = node.data;
+		
+		if ( node.left == null && node.right == null) {
+			
+			if (pathLen == height && (flag ==0 || flag == 1)) {
+				printArray(path, pathLen, flag);
+				flag = 2;
+			}
+			return;
+		}
+		
+		printPath(node.left, path, pathLen, height);
+		printPath(node.right, path, pathLen, height);
+
+	}
+	
+	static void printArray(int[] path, int len, int flag ) {
+		
+		for ( int i=0; i < len; i++) {
+			System.out.println(path[i]);
+		}
+		System.out.println(" printed ");
+	}
+	
+	
+	
+	static Node diameter_root = null;
+	static int leftHeight = 0;
+	static int rightHeight = 0;
+	static int ans = 0;
 	
 	static int diameter( Node root ) {
 		
@@ -54,6 +105,13 @@ public class DiameterOfATree {
 		int rd = findDiameter(root.right, rh);
 		
 		h.height = 1 + Math.max(lh.height, rh.height);
+		
+		if ( ans < 1 + lh.height + rh.height) {
+			ans = 1 + lh.height + rh.height;
+			leftHeight = lh.height;
+			rightHeight = rh.height;
+			diameter_root = root;
+		}
 		
 		return Math.max(1 + lh.height + rh.height, Math.max(ld, rd));
 	}
