cb7670119efa9a45e364d77955b2fdb1660642fe
Jilani <sjilani2014@gmail.com>
2020-04-24 16:49:56 +0530

    BT: check for symmetric trees

--- ProblemSolving/src/com/jilani/trees/checkandprint/CheckForSymmetric.java
+++ ProblemSolving/src/com/jilani/trees/checkandprint/CheckForSymmetric.java
@@ -3,7 +3,7 @@ package com.jilani.trees.checkandprint;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class CheckForMirrorSubtrees {
+public class CheckForSymmetric {
 
 	public static void main(String[] args) {
 
