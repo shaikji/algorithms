c065ab09272ae34333b0ffb24a2e7fe855af1fbf
Jilani <sjilani2014@gmail.com>
2020-04-25 15:23:58 +0530

    BT: Find max root to leaf sum

---
+++ ProblemSolving/src/com/jilani/trees/summation/FindMaxPathSumFromRootToLeaf.java
@@ -0,0 +1,61 @@
+package com.jilani.trees.summation;
+
+public class FindMaxPathSumFromRootToLeaf {
+
+	public static void main(String[] args) {
+
+		root = new Node(-15);
+		root.left = new Node(5);
+		root.right = new Node(6);
+
+		root.left.left = new Node(-8);
+		root.left.right = new Node(1);
+		root.right.left = new Node(3);
+		root.right.right = new Node(9);
+
+		root.left.left.left = new Node(2);
+		root.left.left.right = new Node(6);
+		
+		root.right.right.right = new Node(0);
+		
+		root.right.right.right.left = new Node(4);
+		root.right.right.right.right = new Node(-1);
+		root.right.right.right.right.left = new Node(10);
+		
+		maxPathSum(root,0);
+		
+		System.out.println(" Sum = " + maxsum );
+	}
+	
+	
+	static void maxPathSum( Node root, int sum) {
+		
+		if ( root == null)
+			return;
+		
+		sum += root.data;
+		
+		if ( root.left == null && root.right == null) {
+			if ( maxsum < sum)
+				maxsum = sum;
+			return;
+		}
+		
+		maxPathSum(root.left, sum);
+		maxPathSum(root.right, sum);
+	}
+	
+	static int maxsum = Integer.MIN_VALUE;
+
+	static Node root;
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+}
