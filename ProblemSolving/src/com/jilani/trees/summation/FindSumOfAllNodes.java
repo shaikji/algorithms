0481d78379ecc3b1bf958d576ce14555f0f13ab8
Jilani <sjilani2014@gmail.com>
2020-04-25 08:07:22 +0530

    BT: Sum of all nodes in a Binary Tree

---
+++ ProblemSolving/src/com/jilani/trees/summation/FindSumOfAllNodes.java
@@ -0,0 +1,50 @@
+package com.jilani.trees.summation;
+
+public class FindSumOfAllNodes {
+
+	public static void main(String[] args) {
+
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+
+		root.left.left.left = new Node(8);
+		root.left.left.right = new Node(9);
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(11);
+		root.right.left.left = new Node(12);
+		root.right.left.right = new Node(13);
+		root.right.right.left = new Node(14);
+		root.right.right.right = new Node(15);
+		
+		System.out.println(" Sum = " + sum(root));
+	}
+	
+	
+	static int sum( Node root) {
+		
+		if ( root == null)
+			return 0;
+		
+		return root.data + sum(root.left) + sum(root.right);
+	}
+	
+	
+
+	static Node root;
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+}
