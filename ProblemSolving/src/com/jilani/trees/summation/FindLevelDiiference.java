d3d868c0c20c2546181541221f53d5de82bdb30d
Jilani <sjilani2014@gmail.com>
2020-04-26 17:44:50 +0530

    BT: Find difference of odd and even levels

---
+++ ProblemSolving/src/com/jilani/trees/summation/FindLevelDiiference.java
@@ -0,0 +1,64 @@
+package com.jilani.trees.summation;
+
+public class FindLevelDiiference {
+
+	public static void main(String[] args) {
+
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		
+		
+		System.out.println(" Sum = " + findDiff(root));
+	}
+	
+	static int findDiff(Node root) {
+		
+		if (root == null)
+			return 0;
+		
+		
+		return root.data - findDiff(root.left) - findDiff(root.right);
+	}
+	
+	static int sumNodes(Node root, int max) {
+		
+		if ( root == null)
+			return 0;
+		
+		if ( max == 1) {
+			return root.data;
+		}
+		
+		return sumNodes(root.left, max-1) + sumNodes(root.right, max-1);
+	}
+	
+	static int maxDepth(Node root) {
+		
+		if ( root == null)
+			return 0;
+		
+		int ld = maxDepth(root.left);
+		int rd = maxDepth(root.right);
+		
+		return 1 + Math.max(ld, rd);
+	}
+	
+
+	static Node root;
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+}
