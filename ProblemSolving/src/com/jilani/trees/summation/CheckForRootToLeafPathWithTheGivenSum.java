9762df0aa57d00947ac37199efb5b79bc5d201d4
Jilani <sjilani2014@gmail.com>
2020-04-29 09:22:54 +0530

    BT: Check if there is any root to leaf path with the given sum

---
+++ ProblemSolving/src/com/jilani/trees/summation/CheckForRootToLeafPathWithTheGivenSum.java
@@ -0,0 +1,51 @@
+package com.jilani.trees.summation;
+
+public class CheckForRootToLeafPathWithTheGivenSum {
+
+	public static void main(String[] args) {
+		
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+
+		root.left.left.left = new Node(8);
+		root.left.left.right = new Node(9);
+
+		int K = 8;
+		System.out.println("hasSum "+ K + " = " + hasSum(root,  K, 0));
+	}
+	
+	static boolean hasSum ( Node root, int K, int sum ) {
+		
+		if ( root == null)
+			return false;
+		
+		sum += root.data;
+		
+		if ( root.left == null && root.right == null) {
+			return sum == K; 
+		}
+		
+		return hasSum(root.left, K, sum) || hasSum(root.right, K, sum);
+	}
+	
+	static Node root;
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	
+
+}
