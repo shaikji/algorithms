828e59957eea8edbbaf49ca3a2d33dd1a20d55fb
Jilani <sjilani2014@gmail.com>
2020-05-04 00:19:40 +0530

    Graphs: Count number of paths from one node to other

---
+++ ProblemSolving/src/com/jilani/graphs/CountPathsFromSrcToDest.java
@@ -0,0 +1,64 @@
+package com.jilani.graphs;
+
+import java.util.LinkedList;
+
+public class CountPathsFromSrcToDest {
+
+	public static void main(String[] args) {
+		
+		Graph g = new Graph(5); 
+        g.addEdge(0, 1); 
+        g.addEdge(0, 2); 
+        g.addEdge(0, 3); 
+        g.addEdge(1, 3); 
+        g.addEdge(2, 3); 
+        g.addEdge(1, 4); 
+        g.addEdge(2, 4); 
+        
+        int src= 0;
+        int dest = 3;
+        
+        System.out.println(" Number of Paths from " + src +" to " + dest + " = " + g.countpaths(src, dest, 0 ));
+
+	}
+
+	static class Graph {
+		int V;
+		LinkedList<Integer>[] adjListArr;
+
+		Graph(int V) {
+
+			this.V = V;
+			adjListArr = new LinkedList[V];
+
+			for (int i = 0; i < V; i++)
+				adjListArr[i] = new LinkedList<Integer>();
+		}
+
+		void addEdge(int u, int v) {
+			if (u >= V || v >= V) {
+				System.out.println(" Please provide valid vertices");
+				return;
+			}
+
+			adjListArr[u].add(v);
+		}
+		
+		
+		int countpaths(int src, int dest, int numpaths) {
+			
+			if ( src == dest) {
+				numpaths++;
+			} else {
+				
+				for ( int i: adjListArr[src]) {
+					numpaths = countpaths(i, dest, numpaths);
+				}
+			}
+			return numpaths;
+		}
+	}
+	
+	
+
+}
