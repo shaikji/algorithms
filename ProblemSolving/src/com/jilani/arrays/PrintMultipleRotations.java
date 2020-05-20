dd4def219936d87ef6654991e51a5ddc5c67da28
Jilani <sjilani2014@gmail.com>
2020-05-20 17:19:27 +0530

    Arrays: Print multiple left or right rotations

--- ProblemSolving/src/com/jilani/arrays/PrintMultipleRotations.java
+++ ProblemSolving/src/com/jilani/arrays/PrintMultipleRotations.java
@@ -11,15 +11,35 @@ public class PrintMultipleRotations {
 		printArray(arr);
 		
 		
-		for ( int i=1; i <=10; i++)
-			printRotation(arr, n, i);
+		System.out.println(" Left Rotations of Array ");
+		for ( int i=1; i <=5; i++)
+			printLeftRotations(arr, n, i);
+		
+		
+		printArray(arr);
+		System.out.println(" Right Rotations of Array ");
+
+		
+		for ( int i=1; i <=5; i++)
+			printRightRotations(arr, n, i);
 		
 	}
 	
 	
-	static void printRotation(int[] arr, int n, int k) {
+	static void printLeftRotations(int[] arr, int n, int k) {
 		
 		for ( int i=k; i < k+n; i++)
+			System.out.print(" " + arr[i%n]);
+		System.out.println("\n");
+	}
+
+	static void printRightRotations(int[] arr, int n, int k) {
+		
+		k = k%n;
+		
+		int start = (n-k);
+		
+		for ( int i=start; i < start + n; i++)
 			System.out.print(" " + arr[i%n]);
 		System.out.println("\n");
 	}
