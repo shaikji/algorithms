b400b3da08aaf127503abb7cb02b34de9a7c51c7
Jilani <sjilani2014@gmail.com>
2020-06-09 18:56:42 +0530

    Algos: DnC - Find Bitonic Point

--- ProblemSolving/src/com/jilani/algos/dnc/FindBitonicPoint.java
+++ ProblemSolving/src/com/jilani/algos/dnc/FindBitonicPoint.java
@@ -5,10 +5,24 @@ public class FindBitonicPoint {
 	public static void main(String[] args) {
 		int[] arr = {6, 7, 8, 11, 9, 5, 2, 1};
 
-		System.out.println(" Bitonic Element =  " + findBitonic(arr, 1, arr.length-2));
+		System.out.println(" Bitonic Element =  " + bitonic(arr, arr.length));
 
 	}
 
+	static int bitonic(int[] arr, int n) {
+		if ( arr == null || n <= 0)
+			return -1;
+		if ( n == 1)
+			return arr[0];
+		else if ( n == 2)
+		{
+			if (arr[0] > arr[1])
+				return arr[0];
+			else 
+				return -1;
+		}
+		return findBitonic(arr, 1, n-2);
+	}
 	static int findBitonic(int[] arr, int low, int high) {
 		
 		if( low > high)
